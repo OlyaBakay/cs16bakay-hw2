65c08c4324f07ad0774d1dba53428e334089346e
OlyaBakay <Olhabak@gmail.com>
2017-01-10 05:37:14 +0200

    Update Queue.java

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,29 @@
 package ua.edu.ucu.collections;
 
-public class Queue {
-    
-}
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
+    public class Queue {
+        ImmutableLinkedList test;
+
+        public Queue() {
+            testlist = new ImmutableLinkedList();
+        }
+
+        public Object peek() {
+            return testlist.get(0);
+        }
+
+        public Object dequeue() {
+            Object first = testlist.get(0);
+            testlist.remove(0);
+            return first;
+        }
+
+        public void enqueue(Object e) {
+            testlist.add(0, e);
+        }
+
+        public String toString(){
+            return testlist.toString();
+        }
+    }
