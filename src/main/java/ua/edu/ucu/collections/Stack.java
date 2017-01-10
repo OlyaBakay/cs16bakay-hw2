c45ffa34e415d4e11f0ca09682742e16bf65b802
OlyaBakay <Olhabak@gmail.com>
2017-01-10 05:44:53 +0200

    Update Stack.java

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,30 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableArrayList;
+
 public class Stack {
+
+    ImmutableArrayList testlist;
     
+    public Stack() {
+        testlist = new ImmutableLinkedList();
+    }
+    public Object peek() {
+        return testlist.get(testlist.size()-1);
+    }
+
+    public Object pop() {
+        Object removed = peek();
+        remove(stack.array.length - 1);
+        return removed;
+    }
+
+     public void pop(Object e) {
+        this.testlist = testlist.add(e);
+    }
+
+    public String toString() {
+        return testlist.toString();
+    }
+
 }
